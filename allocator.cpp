818be55f4079aa45be80efa5a0737628591d0099
Нина Камкия <ninakamkia@MacBook-Air-NinelleKam.local>
2021-03-25 13:36:50 +0300

    makefiles + test

--- allocator.cpp
+++ allocator.cpp
@@ -1,26 +1,28 @@
-#include "allocator.hpp"
+#include "test.hpp"
 
 int main() {
-	std::ios_base::sync_with_stdio(false);
-	std::cin.tie(NULL);
+	test();
+	// std::ios_base::sync_with_stdio(false);
+	// std::cin.tie(NULL);
 
-	std::string s;
-	size_t size;
-	std::cin >> s;
-	Allocator alloc;
-	while (std::cin >> s) {
-		if (s == "makeAllocator") {
-			std::cin >> size;
-			alloc.makeAllocator(size);
-		}
-		if (s == "alloc") {
-			std::cin >> size;
-			std::cout << "alloc result " << alloc.alloc(size) << "\n";
-		}
-		if (s == "reset") {
-			alloc.reset();
-		}
-	}
-	std::cout << "Exit!";
+	// std::string s;
+	// size_t size;
+	// std::cin >> s;
+	// Allocator alloc;
+	// while (std::cin >> s) {
+	// 	if (s == "makeAllocator") {
+	// 		std::cin >> size;
+	// 		alloc.makeAllocator(size);
+	// 	}
+	// 	if (s == "alloc") {
+	// 		size = 0;
+	// 		std::cin >> size;
+	// 		alloc.alloc(size);
+	// 	}
+	// 	if (s == "reset") {
+	// 		alloc.reset();
+	// 	}
+	// }
+	// std::cout << "Exit!";
 	return (0);
 }
--- allocator.hpp
+++ allocator.hpp
@@ -6,43 +6,20 @@
 #include <unistd.h>
 #include <string>
 
-static char *ptr = NULL;
-static size_t maxSize = 0;
-static size_t current_ptr_size = 0;
 
 class Allocator
 {
+private:
+	char *ptr;
+	size_t maxSize;
+	size_t current_ptr_size;
 public:
+	Allocator(size_t size);
     void makeAllocator(size_t maxSize);
     char* alloc(size_t size);
     void reset();
+	void information();
 	~Allocator();
 };
-
-void Allocator::makeAllocator(size_t maxSize) {
-	if (maxSize > 0)
-		ptr = new char[maxSize];
-}
-
-char* Allocator::alloc(size_t size) {
-	if (size > maxSize - current_ptr_size || ptr == NULL)
-		return (NULL);
-	current_ptr_size += size;
-	size_t offset = current_ptr_size - size;
-	if (offset < 0 )
-		return (NULL);
-	return (ptr + offset);
-}
-
-void Allocator::reset() {
-	current_ptr_size = 0;
-}
-
-Allocator::~Allocator() {
-	if (ptr != NULL)
-		free(ptr);
-	ptr = NULL;
-	current_ptr_size = 0;
-}
 
 #endif
